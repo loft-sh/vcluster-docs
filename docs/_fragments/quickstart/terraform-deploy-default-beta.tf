0e79bb37e1728298c34d71631f9b0534a00ebbdd
Aimee Ukasick <aimeeu.opensource@gmail.com>
2024-04-23 14:52:56 -0500

    DOC-42 Add version to Helm chart

---
+++ docs/_fragments/quickstart/terraform-deploy-default-beta.tf
@@ -0,0 +1,15 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "my_vcluster" {
+  name             = "my-vcluster"
+  namespace        = "team-x"
+  create_namespace = true
+
+  repository       = "https://charts.loft.sh"
+  chart            = "vcluster"
+  version          = "0.20.0-beta.1"
+}
