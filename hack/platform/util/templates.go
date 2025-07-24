ffb9681ce1177100c4a0f2750505c14e9908ceb9
Piotr Zaniewski <piotr.zaniewski@loft.sh>
2025-07-24 14:42:50 +0200

    feat: remove pro label from right hand side config menu (#909)
    feat: remove pro label from right hand side config menu
    * chore: re-generate config for latest
    * chore: re-generate config for 0.26
    * chore: remove label on imported platform config

--- hack/platform/util/templates.go
+++ hack/platform/util/templates.go
@@ -4,7 +4,7 @@ const TemplateConfigField = `
 <details className="config-field" data-expandable="%t"%s>
 <summary>
 
-%s` + "`%s`" + ` <span className="config-field-required" data-required="%t">required</span> <span className="config-field-type">%s</span> <span className="config-field-default">%s</span> <span className="config-field-enum">%s</span> <span data-pro="%t" className="config-field-pro">pro</span> {#%s}
+%s` + "`%s`" + ` <span className="config-field-required" data-required="%t">required</span> <span className="config-field-type">%s</span> <span className="config-field-default">%s</span> <span className="config-field-enum">%s</span>%s {#%s}
 
 %s
 
