a2e38b4de1a9cabaa02fd1bfbaa84037d8548c1a
Piotr Zaniewski <piotr.zaniewski@loft.sh>
2025-09-15 11:38:34 +0200

    chore: mark 0.24 as EOL in its branch (#1161)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -205,8 +205,8 @@ const config = {
             badge: true,
           },
           "0.24.0": {
-            label: "v0.24 (EOS)",
-            banner: "none",
+            label: "v0.24 (EOL)",
+            banner: "unmaintained",
             badge: true,
           },
           "0.23.0": {
