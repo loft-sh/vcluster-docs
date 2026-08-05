c1c2a373a6bc7a9f373dd6e084047e496c03251a
Piotr <piotr.zaniewski@loft.sh>
2026-08-05 13:33:32 +0200

    fix(docs): prevent archive indexing (#2538)
    Add page and response-level noindex signals so search engines stop serving end-of-life archive content.
    
    Refs DOC-1675

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,7 @@ const config = {
 
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
+  noIndex: true,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
